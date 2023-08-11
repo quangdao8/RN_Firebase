1cb93b647ef73b6b440b32ddaa4b2ce4ed16a35b
Phong <phuongnv@mor.com.vn>
2023-08-11 16:39:01 +0700

    init prj

---
+++ App.tsx
@@ -0,0 +1,133 @@
+import React, {useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  StatusBar,
+  FlatList,
+} from 'react-native';
+import database, {firebase} from '@react-native-firebase/database';
+
+export default function App() {
+  const [inputTextValue, setInputTextValue] = useState(null);
+  const [list, setList] = useState(null);
+
+  useEffect(() => {
+    getDatabase();
+  }, []);
+
+  const getDatabase = async () => {
+    try {
+      // const data = await database().ref('todo').once('value');
+      const data = await firebase
+        .app()
+        .database(
+          'https://rn-auth-7c5ee-default-rtdb.asia-southeast1.firebasedatabase.app/',
+        )
+        .ref('todo')
+        .on('value', tempData => {
+          console.log('aaa', tempData.val());
+          setList(tempData.val());
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleAddData = async () => {
+    try {
+      const index = list?.length || 0;
+      const response = await firebase
+        .app()
+        .database(
+          'https://rn-auth-7c5ee-default-rtdb.asia-southeast1.firebasedatabase.app/',
+        )
+        .ref(`todo/${index}`)
+        .set({
+          value: inputTextValue,
+        });
+
+      console.log(response);
+
+      setInputTextValue('');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden={true} />
+      <View>
+        <Text style={{textAlign: 'center', fontSize: 20, fontWeight: 'bold'}}>
+          Todo App
+        </Text>
+        <TextInput
+          style={styles.inputBox}
+          placeholder="Enter Any Value"
+          value={inputTextValue}
+          onChangeText={value => setInputTextValue(value)}
+        />
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => handleAddData()}>
+          <Text style={{color: '#fff'}}>Add</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.cardContainer}>
+        <Text style={{marginVertical: 20, fontSize: 20, fontWeight: 'bold'}}>
+          Todo List
+        </Text>
+
+        <FlatList
+          data={list}
+          renderItem={item => {
+            if (item.item !== null) {
+              return (
+                <View style={styles.card}>
+                  <Text>{item.item.value}</Text>
+                </View>
+              );
+            }
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+const {height, width} = Dimensions.get('screen');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  inputBox: {
+    width: width - 30,
+    borderRadius: 15,
+    borderWidth: 2,
+    marginVertical: 10,
+    padding: 10,
+  },
+  addButton: {
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 50,
+  },
+  cardContainer: {
+    marginVertical: 20,
+  },
+  card: {
+    backgroundColor: '#fff',
+    width: width - 40,
+    padding: 20,
+    borderRadius: 30,
+    marginVertical: 10,
+  },
+});
